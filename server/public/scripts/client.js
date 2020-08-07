c4d6de5f5c6278af381d69c1a72dbb29c9080016
Paul Buysse <paulbuysse001@gmail.com>
2020-08-07 18:36:20 -0500

    added about half of get route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,35 @@
-$(document).ready(onReady());
+$(document).ready(onReady);
 
 function onReady () {
     console.log('js jq');
+
+    addClickers();
+    getTasks();
+};
+
+function addClickers() {
+    $('#inputArea').on('click', '#addTaskBtn', addTask);
+}
+
+function addTask () {
+    console.log('adding task');
+
+
+};
+
+function getTasks () {
+    console.log('getting tasks')
+
+    $.ajax({
+        method: 'GET',
+        url: '/tasks',
+    }).then(function (response) {
+        console.log('in getTasks GET', response);
+
+        $('#taskTable').empty();
+
+        
+    }).catch(function (error) {
+        console.log('problem getting tasks', error);
+    })
 };
--- server/routes/task-router.js
+++ server/routes/task-router.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 const pool = require('../modules/pool');
+
+router.get('/', (req, res) => {
+    let queryText = `
+    SELECT * FROM "task_list"
+    ORDER BY "id";`;
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error in pool.query GET', error);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
