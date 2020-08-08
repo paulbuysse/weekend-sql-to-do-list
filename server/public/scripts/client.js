95dbdefa6365e1550031747ca1a43ebeb9bdd322
Paul Buysse <paulbuysse001@gmail.com>
2020-08-07 19:01:24 -0500

    linked db and appending to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -26,9 +26,31 @@ function getTasks () {
     }).then(function (response) {
         console.log('in getTasks GET', response);
 
-        $('#taskTable').empty();
+        // $('#taskTable').empty();
 
-        
+        for (let i = 0; i < response.length; i++) {
+            let task = response[i];
+            let completedStatus = ''
+
+            console.log(task.task);
+
+            if (task.completed === false) {
+                completedStatus = 'Not done!'
+            } else if (task.completed === true) {
+                completedStatus = 'Done!'
+            };
+
+
+
+            $('#taskTable').append(`<tr>
+            <th>${i + 1}</th>
+            <td>${task.task}</td>
+            <td>${completedStatus}</td>
+            <td><button>&check;</button></td>
+            <td><button>DELETE</button></td>
+            </tr>
+            `)
+        }
     }).catch(function (error) {
         console.log('problem getting tasks', error);
     })
